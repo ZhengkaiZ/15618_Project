74f4064de438d3f1c7e52787d250c1a06cc47762
ZoeyHou <775165679@qq.com>
2019-04-18 20:47:33 -0400

    Add cuda util funcs

---
+++ cuda/util.c
@@ -0,0 +1,32 @@
+#include "util.h"
+
+static float softmax(float *input, int input_len) {
+    int i;
+    float m;
+    /* Find maximum value from input array */
+    m = input[0];
+    for (i = 1; i < input_len; i++) {
+        if (input[i] > m) {
+            m = input[i];
+        }
+    }
+
+    float sum = 0;
+    for (i = 0; i < input_len; i++) {
+        sum += expf(input[i]-m);
+    }
+
+    for (i = 0; i < input_len; i++) {
+        input[i] = expf(input[i] - m - log(sum));
+    }
+}
+
+static float sigmoid(float x) {
+    float exp_value;
+    float return_value;
+
+    exp_value = exp((double) -x);
+    return_value = 1 / (1 + exp_value);
+
+    return return_value;
+}
